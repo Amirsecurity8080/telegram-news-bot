63d22fd69a28e99d96900e11424e875ad06bb259
emir <141667231+Amirsecurity8080@users.noreply.github.com>
2026-07-28 22:40:21 +0330

    Update send_news.py

--- send_news.py
+++ send_news.py
@@ -1,11 +1,10 @@
+import os
 import feedparser
 import requests
-import os
 
-BOT_TOKEN = os.environ["8938523902:AAHNQknqLLk1DWmhHdwF0ZEDCq4F-PP3_jU"]
-CHANNEL_ID = os.environ["-1003274837538"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHANNEL_ID = os.environ["CHANNEL_ID"]
 
-# می‌تونی چند تا RSS اضافه کنی
 RSS_FEEDS = [
     "https://www.isna.ir/rss",
 ]
@@ -14,22 +13,19 @@ message = "📰 خلاصه اخبار\n\n"
 
 for feed_url in RSS_FEEDS:
     feed = feedparser.parse(feed_url)
-    for entry in feed.entries[:5]:  # ۵ خبر اول هر فید
+    for entry in feed.entries[:5]:
         title = entry.title
         link = entry.link
-        message += f"🔹 {title}\n{link}\n\n"
+        message += "🔹 " + title + "\n" + link + "\n\n"
 
-# تلگرام محدودیت طول پیام داره (۴۰۹۶ کاراکتر)
 if len(message) > 4000:
     message = message[:4000] + "..."
 
-response = requests.post(
-    f"https://api.telegram.org/bot{8938523902:AAHNQknqLLk1DWmhHdwF0ZEDCq4F-PP3_jU}/sendMessage",
-    data={
-        "chat_id": -1003274837538,
-        "text": message,
-        "disable_web_page_preview": False
-    }
-)
+url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage"
+response = requests.post(url, data={
+    "chat_id": CHANNEL_ID,
+    "text": message,
+    "disable_web_page_preview": False
+})
 
 print(response.status_code, response.text)
